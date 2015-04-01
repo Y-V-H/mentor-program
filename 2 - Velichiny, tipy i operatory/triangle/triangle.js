65f43de27b4e739ea3de168626364ec733358c57
Yahor Hurynovich <gyrinovich@gmail.com>
2015-04-01 08:53:54 +0200

    First task

---
+++ 2 - Velichiny, tipy i operatory/triangle/triangle.js
@@ -0,0 +1,21 @@
+/*Triangle
+------------------------*/
+for (var tr = "#"; tr < "########"; tr +="#")
+console.log(tr);
+
+
+
+
+
+
+// for (var cur = 1; cur < 100; cur ++ ){
+// 	if (cur % 3 == 0)
+// 		console.log("Fizz");
+// 	else if (cur % 5 == 0)
+// 		console.log("Buzz");
+// 	else if
+// 		console.log(cur);
+// };
+
+// console.log(cur);
+
