aa057c5dd694668deaff6b2dc066bb983cfd940f
Yahor Hurynovich <gyrinovich@gmail.com>
2015-04-02 00:09:42 +0200

    Task-2 (Fizz and Buzz)

--- 2 - Velichiny, tipy i operatory/triangle/triangle.js
+++ 2 - Velichiny, tipy i operatory/triangle/triangle.js
@@ -1,21 +1,41 @@
-/*Triangle
+/*Task - 1 (Triangle)
 ------------------------*/
-for (var tr = "#"; tr < "########"; tr +="#")
-console.log(tr);
+// for (var tr = "#"; tr < "########"; tr +="#")
+// console.log(tr);
 
 
-
-
-
-
+/*Task - 2 
+If we have 0 after division by three we seeing "Fizz" and 
+after division by five we seeing "Buzz"
+------------------------*/
 // for (var cur = 1; cur < 100; cur ++ ){
 // 	if (cur % 3 == 0)
 // 		console.log("Fizz");
-// 	else if (cur % 5 == 0)
+// 	else if(cur % 5 == 0)
 // 		console.log("Buzz");
-// 	else if
+// 	else
 // 		console.log(cur);
 // };
 
-// console.log(cur);
 
+/*Task - 2.1
+If we have 0 after division by three and five we seeing "FizzBuzz"
+------------------------*/
+for (var cur = 1; cur < 100; cur ++ ){
+	if (cur % 3 == 0&&cur % 5 == 0 )
+		console.log("FizzBuzz");
+	else 
+		console.log(cur);
+};
+
+
+/*Task - 3 (Chess)
+------------------------*/
+// for (var chess = 1; chess < 64; chess++) {
+// 	if ( chess % 2 !==0)
+// 		for (var chess_1; chess_1 < 8; chess_1++){
+			
+// 		}
+// 	else
+// 		console.log(chess);
+// };
