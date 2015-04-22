125b0b88e4098295f4cd1b0d8885704e3cf0ad42
Yahor Hurynovich <gyrinovich@gmail.com>
2015-04-22 12:19:40 +0200

    Task - functions

--- 3 - Function/function.js
+++ 3 - Function/function.js
@@ -1,17 +1,46 @@
 /* 1 - Function min
 ----------------------*/
-var min = function (f1, f2){
+// var min = function (f1, f2){
 
-	// return (Math.min(f1,f2));
-	if (f1 >> f2 )
-		return f2;
-	else if 
-		(f1 << f2 )
-		return f1;
-	else
-		return ("error");
+// 	// return (Math.min(f1,f2));
+// 	if (f1 >= f2 )
+// 		return f2;
+// 	else
+// 		return f1;
+// };
 
+// console.log(min(10, 0));
+
+/* 2 - Function counting B
+----------------------*/
+// var countBs = function (name){
+// 	var count = 0;
+// 	var nameLength = name.length;
+// 	for (var i = 0; i < nameLength; i++) {
+// 		if (name.charAt(i) === "B")
+// 			count++;
+// 	};
+// 	console.log(count);
+// };
+
+// countBs('Bar-B in Briston BB');
+
+/* 2.1 - Function counting character
+----------------------*/
+
+ 
+
+var countChar = function (){
+	var expression = (prompt('Введите выражение на английском языке?', "")); 
+	var character = (prompt('Что искать?', ""));
+	var count = 0;
+	var expressionLength = expression.length;
+
+	for (var i = 0; i < expressionLength; i++) {
+		if (expression.charAt(i) === "character")
+			count++;
+	};
+
+	console.log(count);
 };
-
-console.log(min(0, -1));
-
+countChar();
