34856fe10e76acf7bde90aae5f5eb4503e00763d
Yahor Hurynovich <gyrinovich@gmail.com>
2015-05-26 08:17:33 +0200

    Sea fight 1.0

---
+++ 8 - DOM/dom.js
@@ -0,0 +1,7 @@
+function intil() {
+    var planet = document.getElementByID('greenplanet');
+    planet.innerHTML = "Red Alert: hit by phaser fire!";
+    planet.setAttribute("class", "redtext");
+};
+
+
