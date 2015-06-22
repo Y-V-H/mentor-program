5d96a68c5cee5d3c63a85b5fb2552cd6a0a0c56a
Yahor Hurynovich <gyrinovich@gmail.com>
2015-06-22 13:20:21 +0300

    Events 1.1

--- 10 - Events/events.js
+++ 10 - Events/events.js
@@ -1,25 +1,32 @@
-window.onload = init;
+// window.onload = init;
 
-function init() {
-	var images = document.getElementsByTagName("img");
-		for (var i = 0; i < images.length; i++) {
-		// images[i].onclick = showAnswer;
-		images[i].onmouseover = showAnswer;
-    }
-}
+// function init() {
+// 	var images = document.getElementsByTagName("img");
+// 		for (var i = 0; i < images.length; i++) {
+// 		// images[i].onclick = showAnswer;
+// 		images[i].onmouseover = showAnswer;
+//     }
+// }
 
-function showAnswer(eventObj) {
-	var image = eventObj.target;
+// function showAnswer(eventObj) {
+// 	var image = eventObj.target;
 
-	var name = image.id;
-	name = name + ".jpg";
-	image.src = name;
+// 	var name = image.id;
+// 	name = name + ".jpg";
+// 	image.src = name;
 
-	setTimeout(reblur, 2000, image);
-}
+// 	setTimeout(reblur, 2000, image);
+// }
 
-function reblur(image) {
-	var name = image.id;
-	name = name + "blur.jpg";
-	image.src = name;
-}
+// function reblur(image) {
+// 	var name = image.id;
+// 	name = name + "blur.jpg";
+// 	image.src = name;
+// }
+
+var addOne = function(x) {
+	return x + 1;
+};
+console.log("first " + addOne);
+var six = addOne(5);
+console.log("second " + six);
